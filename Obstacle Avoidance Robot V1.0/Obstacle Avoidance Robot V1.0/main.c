54f352620231d9e2f27f86b38615ed353a43058f
Elpolaky <kareempolaky979@gmail.com>
2023-05-18 20:27:38 +0300

    l

---
+++ Obstacle Avoidance Robot V1.0/Obstacle Avoidance Robot V1.0/main.c
@@ -0,0 +1,28 @@
+
+
+#include "APP/app.h"
+#define F_CPU 8000000UL
+
+int main(void)
+{
+	
+  app_Init();
+  //DIO_INITPIN(pind4,OUTPUT);
+// start_stage();
+ while(1){
+ //pwm(30);
+//CAR_no_obstacle_less_70();
+CAR_no_obstacle_between_30_and_70();
+
+
+
+
+}
+}
+  
+
+  
+  
+  
+  
+
